697d8702a84525fdc6e1fb98a347d356f988b0ee
Geraldo Luiz Yoshizawa <geraldoy@gmail.com>
2018-10-26 10:35:00 -0300

    vr.0.2
    New Create function
    New fromExistingPhrase function

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ router.get("/create",function(req,res){
   console.log(seed.address); // '3Mr5af3Y7r7gQej3tRtugYbKaPr5qYps2ei'
   console.log(seed.keyPair); // { privateKey: 'HkFCbtBHX1ZUF42aNE4av52JvdDPWth2jbP88HPTDyp4', publicKey: 'AF9HLq2Rsv2fVfLPtsWxT7Y3S9ZTv6Mw4ZTp8K8LNdEp' }
 
-  //res.sendFile(path + "about.html");
+  res.send(seed);
 });
 
 router.get("/encrypt/:p/:phrase",function(req,res){
@@ -56,10 +56,19 @@ router.get("/encrypt/:p/:phrase",function(req,res){
 
   res.send(encrypted); // 'U2FsdGVkX1+5TpaxcK/eJyjht7bSpjLYlSU8gVXNapU3MG8xgWm3uavW37aPz/KTcROK7OjOA3dpCLXfZ4YjCV3OW2r1CCaUhOMPBCX64QA/iAlgPJNtfMvjLKTHZko/JDgrxBHgQkz76apORWdKEQ=='
 
-  const restoredPhrase = Waves.Seed.decryptSeedPhrase(encrypted, password);
-  console.log(restoredPhrase); // 'hole law front bottom then mobile fabric under horse drink other member work twenty boss '
+  //const restoredPhrase = Waves.Seed.decryptSeedPhrase(encrypted, password);
+  //console.log(restoredPhrase); // 'hole law front bottom then mobile fabric under horse drink other member work twenty boss '
+});
 
-  //res.sendFile(path + "contact.html");
+router.get("/fromExistingPhrase/:p/:phrase",function(req,res){
+  var password = req.params.p;
+  var phrase = req.params.phrase;
+  const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
+  const seed = Waves.Seed.fromExistingPhrase(phrase);
+  console.log(seed.phrase); // 'hole law front bottom then mobile fabric under horse drink other member work twenty boss'
+  console.log(seed.address); // '3Mr5af3Y7r7gQej3tRtugYbKaPr5qYps2ei'
+  console.log(seed.keyPair); // { privateKey: 'HkFCbtBHX1ZUF42aNE4av52JvdDPWth2jbP88HPTDyp4', publicKey: 'AF9HLq2Rsv2fVfLPtsWxT7Y3S9ZTv6Mw4ZTp8K8LNdEp' }
+  res.send(seed);
 });
 
 router.get("/balance",function(req,res){
@@ -83,7 +92,7 @@ router.get("/balance2/:p/:addr",function(req,res){
   }
 });
 
-router.get("/transfer/:p/:encr/:rec",function(req,res){
+router.get("/transfer/:p/:encr/:rec/:amount",function(req,res){
   const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
   const password = req.params.p;
   const encrypted = req.params.encr;
@@ -98,7 +107,7 @@ router.get("/transfer/:p/:encr/:rec",function(req,res){
     // ID of a token, or WAVES
     assetId: 'CeNAju9EtKveEHutRqCjvy1xkhuSgxu8vn6qidQa62s6',
     // The real amount is the given number divided by 10^(precision of the token)
-    amount: 10000000,
+    amount: req.params.amount,
     // The same rules for these two fields
     feeAssetId: 'CeNAju9EtKveEHutRqCjvy1xkhuSgxu8vn6qidQa62s6',
     fee: 1,
